f1dc335aadc4e2676da1950481c87b5f9eb856a6
Ken Yourek <ken@yourek.net>
2020-03-18 08:17:28 -0500

    Use app settings configuration.

--- sln/Domore.Release.Console/Program.cs
+++ sln/Domore.Release.Console/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 
 namespace Domore {
+    using Conf;
+    using CONF = Conf.Conf;
+
     internal class Program {
         private static void Main(string[] args) {
             try {
+                CONF.Container.ContentsProvider = new AppSettingsProvider();
+                CONF.Container.ConfigureLogging();
                 new Release(args);
             }
             catch (Exception ex) {
